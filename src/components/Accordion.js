60ad425a0ec0ce6d5f1974d82f1b458183779231
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-09 10:50:07 -0300

    Display icons in Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,30 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  const handleClick = (index) => {
+    setExpandedIndex(index);
+  };
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
 
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
+
   return <div>{renderedItems}</div>;
 }
 
