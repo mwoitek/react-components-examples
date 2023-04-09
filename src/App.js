d10553332d5137e43d4ae5f090baa1f384a21697
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-08 21:02:59 -0300

    Add icons to buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 import Button from './components/Button';
 
 function App() {
   return (
     <div>
       <div>
-        <Button success rounded outline>
+        <Button secondary rounded outline>
+          <GoBell />
           Click me!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy now!
         </Button>
       </div>
       <div>
-        <Button warning>See deal!</Button>
+        <Button warning>
+          <GoDatabase />
+          See deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
--- src/components/Button.js
+++ src/components/Button.js
@@ -10,20 +10,29 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = className('px-3', 'py-1.5', 'border', {
-    'border-blue-500 bg-blue-500 text-white': primary,
-    'border-gray-900 bg-gray-900 text-white': secondary,
-    'border-green-500 bg-green-500 text-white': success,
-    'border-yellow-400 bg-yellow-400 text-white': warning,
-    'border-red-500 bg-red-500 text-white': danger,
-    'rounded-full': rounded,
-    'bg-white': outline,
-    'text-blue-500': outline && primary,
-    'text-gray-900': outline && secondary,
-    'text-green-500': outline && success,
-    'text-yellow-400': outline && warning,
-    'text-red-500': outline && danger,
-  });
+  const classes = className(
+    'flex',
+    'items-center',
+    'px-3',
+    'py-1.5',
+    'border',
+    {
+      'border-blue-500 bg-blue-500': primary,
+      'border-gray-900 bg-gray-900': secondary,
+      'border-green-500 bg-green-500': success,
+      'border-yellow-400 bg-yellow-400': warning,
+      'border-red-500 bg-red-500': danger,
+      'rounded-full': rounded,
+      'bg-white': outline,
+      'text-white':
+        !outline && (primary || secondary || success || warning || danger),
+      'text-blue-500': outline && primary,
+      'text-gray-900': outline && secondary,
+      'text-green-500': outline && success,
+      'text-yellow-400': outline && warning,
+      'text-red-500': outline && danger,
+    }
+  );
   return <button className={classes}>{children}</button>;
 }
 
