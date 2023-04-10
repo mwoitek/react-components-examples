2c07fe9574073ed8bab721026a17b380faaadcd4
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-09 23:02:08 -0300

    A few small improvements

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     },
   ];
   return (
-    <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+    <Dropdown options={options} value={selection} onChange={handleSelect} />
   );
 }
 
