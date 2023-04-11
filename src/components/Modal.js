12f84e047d5be78086bcd4d68206c0a4ab217d0e
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-11 16:20:56 -0300

    Basic implementation of Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,15 @@
 function Modal() {
-  return <div>Modal</div>;
+  return (
+    <div>
+      <div className='absolute inset-0 bg-gray-300 opacity-80'></div>
+      <div className='absolute inset-40 p-10 bg-white'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eveniet
+        praesentium ea beatae maiores porro alias hic. Doloremque fugiat eos
+        perspiciatis eius ipsum tenetur, exercitationem, ex suscipit sunt
+        incidunt vel?
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
