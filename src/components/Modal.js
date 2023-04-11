1ffa6f9da87b3f77c325cd7b434b639db50caddf
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-11 16:46:12 -0300

    Fix Modal using a portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,7 @@
+import ReactDOM from 'react-dom';
+
 function Modal() {
-  return (
+  return ReactDOM.createPortal(
     <div>
       <div className='absolute inset-0 bg-gray-300 opacity-80'></div>
       <div className='absolute inset-40 p-10 bg-white'>
@@ -8,7 +10,8 @@ function Modal() {
         perspiciatis eius ipsum tenetur, exercitationem, ex suscipit sunt
         incidunt vel?
       </div>
-    </div>
+    </div>,
+    document.querySelector('.modal-container')
   );
 }
 
