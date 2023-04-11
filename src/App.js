3e41d2df8e3066acf50630b53e2aed908d0b3548
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-11 11:19:20 -0300

    Add Link component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-// import { useState } from 'react';
-// import Dropdown from './components/Dropdown';
+import Link from './components/Link';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to='/accordion'>Go to accordion</Link>
+      <Link to='/dropdown'>Go to dropdown</Link>
+    </div>
+  );
 }
 
 export default App;
