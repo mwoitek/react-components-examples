46471a6ae7f3c0e1a22ef5e3fd5edd2976ace5b6
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-11 11:45:38 -0300

    Improve Link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,15 +1,25 @@
 import { useContext } from 'react';
+import classNames from 'classnames';
 import NavigationContext from '../context/navigation';
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  const classes = classNames('text-blue-500');
+
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
