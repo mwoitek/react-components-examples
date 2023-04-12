289d1a67c70114c549d8c609da649495c6615a46
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-11 21:15:04 -0300

    Create table rows dynamically

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -7,7 +7,14 @@ function TablePage() {
     { name: 'Banana', color: 'bg-yellow-500', score: 1 },
     { name: 'Lime', color: 'bg-green-500', score: 4 },
   ];
-  const config = [{ label: 'Fruit' }, { label: 'Color' }, { label: 'Score' }];
+  const config = [
+    { label: 'Name', render: (fruit) => fruit.name },
+    {
+      label: 'Color',
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    { label: 'Score', render: (fruit) => fruit.score },
+  ];
 
   return (
     <div>
