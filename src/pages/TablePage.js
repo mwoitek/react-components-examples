119167ceb280a2ac095e09a2696f025c91c51494
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-14 13:42:26 -0300

    Table wrapup

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -6,6 +6,7 @@ function TablePage() {
     { name: 'Apple', color: 'bg-red-300', score: 3 },
     { name: 'Banana', color: 'bg-yellow-500', score: 1 },
     { name: 'Lime', color: 'bg-green-500', score: 4 },
+    { name: 'Cherry', color: 'bg-red-700', score: 2.5 },
   ];
   const config = [
     {
@@ -21,6 +22,11 @@ function TablePage() {
       label: 'Score',
       render: (fruit) => fruit.score,
       sortValue: (fruit) => fruit.score,
+    },
+    {
+      label: 'Score Squared',
+      render: (fruit) => fruit.score ** 2,
+      sortValue: (fruit) => fruit.score ** 2,
     },
   ];
 
